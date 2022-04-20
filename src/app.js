dc5a3bab36538c214ce148b251e7cd4a41e349d9
gabo180 <gabrieldejesushernandez@gmail.com>
2022-04-20 23:46:01 +0000

    Show all domains in the HTML, NOT in CONSOLE

--- src/app.js
+++ src/app.js
@@ -5,24 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#allDomains").innerHTML = domainNameGenerator();
+  });
+};
+
+const domainNameGenerator = () => {
   let pronoun = ["the", "our", "your"];
   let adj = ["great", "big", "worst"];
   let noun = ["jogger", "racoon", "food", "thing"];
   let ext = [".com", ".net", ".io", ".us"];
 
+  var result = [];
+
   for (let p = 0; p < pronoun.length; p++) {
-    // console.log(pronoun[p]);
     for (let a = 0; a < adj.length; a++) {
-      //   console.log(adj[a]);
       for (let n = 0; n < noun.length; n++) {
-        // console.log(noun[n]);
         for (let e = 0; e < ext.length; e++) {
-          console.log(`${pronoun[p]}${adj[a]}${noun[n]}${ext[e]}`);
+          result.push(`${pronoun[p]}${adj[a]}${noun[n]}${ext[e]}`);
         }
       }
     }
   }
+
+  return result.join("</br>");
 };
 
 //   let getDomainName = () =>
